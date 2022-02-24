982dfd781cf3b3dbe050af4180985c726adc3d49
dcanonmolina <dcanonmolina@twilio.com>
2022-02-24 12:19:01 -0500

    fix webhook url

--- src/services/changeWebhook.js
+++ src/services/changeWebhook.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
-const urlChangeWebhook = "/updateSMSwebhook";
-const urlWebhook = "/inboundMessages";
+const urlChangeWebhook = window.location.origin +"/updateSMSwebhook";
+const urlWebhook =window.location.origin + "/inboundMessages";
 
 const changeWebhook = (pwd, numberID) =>{
     const params = new URLSearchParams();
     params.append('pwd', pwd);
     params.append('SmsUrl', urlWebhook);
     params.append('phoneNumberID', numberID);
-    
+    console.log(urlWebhook)
     const request = axios.post(urlChangeWebhook,params);
     return request.then(response => {
         return {...response.data, "urlNewWebhook": urlWebhook}});
